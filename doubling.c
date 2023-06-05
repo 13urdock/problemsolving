ce919544e083ffcf91e98342b9a7a5b63f12651a
13urdock <uyeong7181@gmail.com>
2023-06-05 22:48:18 +0900

    encoding with entering parameter

--- doubling.c
+++ doubling.c
@@ -1,10 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h> 
+#include <stdlib.h>
 
-int main(int argc, char* argv[]){ 
-	FILE* fp = fopen("double_test.txt", "r");
-	FILE* encode = fopen("encoded_text.txt", "wt"); 
+int check_params(int argc, char* argv[]) {
+	if (argc == 3)
+		return 1;
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	if(check_params(argc, argv) == 0) {
+		printf("Usage: %s should have 2 parameter.\n", argv[0]);
+		exit(1);	
+	}
+
+	FILE* fp = fopen(argv[1], "r");
+	FILE* encode = fopen(argv[2], "wt");
 	FILE* fp2 = fopen("double_test.txt", "r");	
+
+	if (fp ==NULL)
+		printf("failed to open file");
+	if (fp2 == NULL)
+		printf("failed to open file");
+ 
 	char read, read2;
 
 	do{
