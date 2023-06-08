54117a9a9c7c928f6bff602822d373e02e2bd47c
13urdock <uyeong7181@gmail.com>
2023-06-08 23:51:15 +0900

    moved case 2 to item_status function, but need to be fixed in declaration and detecting error in enter key

--- decoding.c
+++ decoding.c
@@ -1,5 +1,3 @@
-decode 아이템 수정 진행중
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,12 +26,14 @@ void user_info(char* user[]){ // 유저 상태 초기화
      user[5] = "MP";
      user[6] = "COIN";
  }
+
  void friend_info(char* friend[]){ // 유저 상태 초기화
      friend[0] = "ID";
      friend[1] = "NAME";
      friend[2] = "GENDER";
      friend[3] = "AGE";
  }
+
  char* separate(char str[2000]){
      for(int i=0; i<9; i++)
          sscanf(str, "%[^\n/]/",buffer[i]);
@@ -48,33 +48,98 @@ void user_info(char* user[]){ // 유저 상태 초기화
      //printf("%ld\n",strlen(str));
      return buffer[8];
  }
- void item_status(Item *item,char str[2000]){
-     char token1[10];
-     char token2[10];
-     int index = 0;
 
-     int i=0;
-     while( sscanf(str+i, "%[^/]/%[^/]/",item[index].name,item[index].data) == 2 ){
+ void item_status(Item *item,char str[2000]){
+      char token1[10];
+      char token2[10];
+      int index = 0;
+
+      int i=0;
+			
+			while( sscanf(str+i, "%[^/]/%[^/]/",item[index].name,item[index].data) == 2 ){
          i += strlen(token1) + strlen(token2) + 2; // 다음 토큰의 시작 위치, / 두개이므로 +2
          index++;
-     }
-     /*
-         아이템 문자열 9개와 아이템 숫자 9를 비교해서
-         가장 많이 나온 문자가 올바른 문자이므로
-         그 구조체를 리턴하는 코드
-     */
- }
- void add_str_arr(char str[2000]){
-     int len = strlen(str); // 문자열의 길이
+      }
+  
+      if (error_count == 0 && str_count < 9) {
+     		  // 9개의 문자열을 저장
+          strcpy(str_buffer[str_count], item->name);
+          str_count++;
+			}
+		 	else if (error_count == 0 && str_count == 9) {
+					// 9개의 문자열을 모두 읽은 경우
+					int  same_count = 0; // 반복되는 문자열 개수
 
-     int i;
-     for(i = 1; i < len; i++){ // /로 시작할 때 한글자씩 앞으로 shift
-         str[i-1] = str[i];
-     }
-     str[len-1] = '\0'; // 문자열의 끝
-     strcpy(str_buf[str_arr_num],str); // 버퍼에 문자열 저장
-     str_arr_num = (str_arr_num+1) % 9; // 0-8번까지만
+					for (int i = 1; i < 9; i++) {
+         		if (strcmp(str_buffer[i], str_buffer[i-1]) == 0) {
+              same_count++;
+         		}  
+						else 
+              same_count = 1;
+
+           	if (same_count >= 6) {
+              // 오류 복구
+              strcpy(error_str, str_buffer[i]);
+              strcpy(error_data, item->data);
+              error_count++;
+              break;
+            }
+         	}
+
+					// '\n' 문자에서 오류가 발생했을 때의 처리를 추가
+          if (strchr(item->name, '\n') || strchr(item->data, '\n')) {
+          	// 오류 개수 증가수 
+						error_count++;
+					}	
+
+          // 첫 번째 문자열과 다른 경우
+          if (strcmp(str_buffer[0], item->name) != 0) {
+            strcpy(str_buffer[0], str_buffer[8]);
+            str_count = 1;
+          }
+     	}
  }
+
+void shift_left(){
+    int len = 0;
+
+    for(int i = 0; i < 9; i++){
+        len = strlen(str_buf[i]); // 문자열의 길이
+        str_buf[i][len-1] = '\0'; // 문자열의 끝의 개행문자를 '\0'으로
+
+        for(int j = 1; j < len; j++){ // '/'든 변형된 문자든 한글자씩 앞으로 shift
+            str_buf[i][j-1] = str_buf[i][j];
+        }
+    }
+//  for(int i=0; i<9; i++)
+//      printf("%s\n",str_buf[i]);
+}
+int is_equal(char str1[1001], char str2[1002]){
+    if( strcmp(str1, str2) == 0 )
+        return 1;
+    return 0;
+}
+
+int check_desc_origin_data(){
+    int count = 0; // 문자열이 같은 배열이 있는 수
+    for(int i=0; i<9; i++){
+        for(int j=0; j < 9; j++){
+            if( is_equal( str_buf[i], str_buf[j] )){
+                count++;
+            }
+        }
+        if( count >= 5 ) // 5번 이상 같으면
+            return i; // i인덱스 출력
+        count = 0; // count 초기화
+    }
+    return -1; // 잘못 출력 시
+}
+
+void add_str_arr(char str[2000]){
+    strcpy(str_buf[str_arr_num],str); // 버퍼에 문자열 저장
+    str_arr_num = (str_arr_num+1) % 9;
+}
+
  void run(FILE *read, FILE *write){
      char *user[2000]; // 유저 상태 기록
      Item item[9]; // 아이템 상태 기록
@@ -133,42 +198,7 @@ void user_info(char* user[]){ // 유저 상태 초기화
                  }
                   else{
                      item_status(item,line);
-                     fprintf(write,"%s: %s\n",item->name, item->data); // 파일에 입력
-
-                     if (error_count == 0 && str_count < 9) {
-                         // 9개의 문자열을 저장
-                         strcpy(str_buffer[str_count], item->name);
-                         str_count++;
-                     } else if (error_count == 0 && str_count == 9) {
-                         // 9개의 문자열을 모두 읽은 경우
-                         int same_count = 1; // 반복되는 문자열 개수
-                         for (int i = 1; i < 9; i++) {
-                             if (strcmp(str_buffer[i], str_buffer[i-1]) == 0) {
-                                 same_count++;
-                             } else {
-                                 same_count = 1;
-                             }
-                             if (same_count >= 6) {
-                                 // 오류 복구
-                                 strcpy(error_str, str_buffer[i]);
-                                 strcpy(error_data, item->data);
-                                 error_count++;
-                                 break;
-                             }
-                         }
-                         // 마지막 문자열까지 모두 같은 경우
-                         if (same_count >= 6 && error_count > 0) {
-                             fprintf(write, "아이템 구조체 오류 복구:\n");
-                             fprintf(write, "반복되는 문자열: %s\n", error_str);
-                             fprintf(write, "반복되는 데이터: %s\n", error_data);
-                             error_count = 0; // 오류 복구 완료
-                         }
-                         // 첫 번째 문자열과 다른 경우
-                         if (strcmp(str_buffer[0], item->name) != 0) {
-                             strcpy(str_buffer[0], str_buffer[8]);
-                             str_count = 1;
-                         }
-                     }
+                     fprintf(write,"%s: %s\n",item->name, item->data); // 파일에 입력 
                  }
                  break;
              case 3:
@@ -195,6 +225,7 @@ void user_info(char* user[]){ // 유저 상태 초기화
          }
      }
  }
+
  int main(int argc, char* argv[]){
      // putty에서 ./decoder.out encoded_data1.modified result1.txt 이런식으로 입력하면
      // argv[1] = encoded_data1.modified
